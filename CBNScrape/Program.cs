f1f239bd237b24d814133dfb7735bee434bc0e22
Ofoedu Frank Ebuka <Frankofoedu@gmail.com>
2021-01-20 02:01:47 +0100

    moved class to new file

--- CBNScrape/Program.cs
+++ CBNScrape/Program.cs
@@ -8,23 +8,6 @@ using System.IO;
 
 namespace CBNScrape
 {
-    public class SecuritiesModel
-    {
-        public string AuctionDate { get; set; }
-        public string SecurityType { get; set; }
-        public string Tenor { get; set; }
-        public string AuctionNo { get; set; }
-        public string Auction { get; set; }
-        public string MaturityDate { get; set; }
-        public string TotalSubscription { get; set; }
-        public string TotalSuccessful { get; set; }
-        public string RangeBid { get; set; }
-        public string SuccessfulBidRate { get; set; }
-        public string Description { get; set; }
-        public string Rate { get; set; }
-        public string TrueYield { get; set; }
-        public string AmountOffered { get; set; }
-    }
     class Program
     {
         private static readonly string BaseUrl = "https://www.cbn.gov.ng";
@@ -61,7 +44,7 @@ namespace CBNScrape
                 listExtract.AddRange(extractedInfo);
             }
 
-
+            //save list of extracted data to excel
             var bytes = listExtract.ToExcel();
            await File.WriteAllBytesAsync("sec.xlsx", bytes);
         }
